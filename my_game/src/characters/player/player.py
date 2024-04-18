d6755ceb51af97f02810fab0ad947d24a9a6725c
Sagimov Aibyn <aibyn.sagimov@gmail.com>
2024-04-19 00:39:59 +0500

    exper

--- my_game/src/characters/player/player.py
+++ my_game/src/characters/player/player.py
@@ -1,10 +1,34 @@
 import pygame
+import os
+
+def load_sprite_sheets(dir1, dir2, width, height, direction = False):
+    path = os.path.join("src", dir1, dir2)
+    images =[f for f in os.listdir(path) if os.path.isfile(join(path, f))]
+    
+    all_sprites = {}
+    
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
         
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i * width, 0, width, height)
+            surface.blit(sprite_sheet, (0, 0), rect)
+            sprites.append(pygame.transform.scale2x(surface))
+        
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "")] = sprites
+            
+    return all_sprites  
         
 class Player(pygame.sprite.Sprite):
     COLOR=(255,0,0)
     GRAVITY = 1
-    SPRITES = load_sprite_sheets("PinkMan", 32, 32, True)
+    SPRITES = load_sprite_sheets("player", "animation", 32, 32, True)
     def __init__(self, x, y, width, height ):
         
         self.rect = pygame.Rect(x, y, width, height)
@@ -40,7 +64,7 @@ class Player(pygame.sprite.Sprite):
         self.fall_count += 1
     
     def draw(self, win):
-        self.sprite = self.SPRITES["idle"][0]
+        self.sprite = self.SPRITES["calm"][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
     
         
